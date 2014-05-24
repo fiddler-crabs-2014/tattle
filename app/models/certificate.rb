c6e8a74527aebe137eb70e233d6c4ebdee959e2c
Tyler Adams <tyleraadams@gmail.com>
2014-05-24 17:55:34 -0400

    Renamed migration folder to migrate. Ran Migrations. Scraped data from FTF and FTUSA to seed database with companies and certifications.
    * db/migration => db/migrate
    * ran db:migrate
    * created Certificate methods to scrape data from FTF and FTUSA
    * added config/ftf_members.html - html file converted from PDF of FTF data
    * seeded database with scraped info

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -1,15 +1,28 @@
+require 'open-uri'
+
 class Certificate < ActiveRecord::Base
   has_many :certifications
   has_many :companies, :through => :certifications
-    def self.get_certs
-      path = File.join(Rails.root, "config", "ftf_members.html")
-      page = Nokogiri::HTML(open(path))
+  
+  def self.ftf_get_certs
+    path = File.join(Rails.root, "config", "ftf_members.html")
+    page = Nokogiri::HTML(open(path))
 
-      body = "Fair Trade Federation"
-      name = "Fair Trade Federation Mempership"
-      category = "fair trade"
+    body = "Fair Trade Federation"
+    name = "Fair Trade Federation Mempership"
+    category = "fair trade"
 
-      @certificate = Certificate.create(body: body, name: name, category: category)
-      page.css('h4').each { |element| @certificate.companies.create( name: element.text) }
-    end
+    @certificate = Certificate.create(body: body, name: name, category: category)
+    page.css('h4').each { |element| @certificate.companies.create( name: element.text) }
+  end
+
+
+  def self.ftusa_get_certs
+    # path = "http://www.fairtradeusa.org/products-partners"
+    page = Nokogiri::HTML(open("http://www.fairtradeusa.org/products-partners").read)
+    @certificate = Certificate.create(body: "Fair Trade USA", name: "Fair Trade USA Licensed Partner", category: "fair trade")
+    page.css('.partner-name a').each { |element| @certificate.companies.create(name: element.text ) }
+  end
+
+
 end
