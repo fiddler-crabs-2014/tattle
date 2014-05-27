c001269d5f577b0d2bb8d28aa84ea7ce5061085f
Adam Godel <null>
2014-05-27 11:39:03 -0400

    fixed bug, parents only coming back if they exist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::Base
   end
 
   def self.freebase_search(company_name)
-    results = {"company" => { name: company_name }, "parents" =>[] }
+    results = {"company" => { name: company_name }, }
     resource = FreebaseAPI::Topic.search(company_name)
     best_match = resource.values.first
     results[:industry] = best_match.as_json["data"]["property"]["/common/topic/notable_for"]["values"][0]["text"]
@@ -40,9 +40,9 @@ class ApplicationController < ActionController::Base
       results["company"][:description] = self.get_description(self.get_id(company_name))
 
       parents = FreebaseAPI.session.mqlread({:id => best_match.id, :'/organization/organization/parent' => [{ :parent => [] }] })
-
+      results["parents"] = [] if parents["/organization/organization/parent"]
       parents["/organization/organization/parent"].each_with_index do |parent, index|
-        results["parents"] << { "parent"+(index+1).to_s => {name: parent['parent'][0], description: self.get_description(get_id(parent['parent'][0]))}} unless parent['parent'][0] == company_name || parent['parent'][0] == nil
+        results["parents"] << {name: parent['parent'][0], description: self.get_description(get_id(parent['parent'][0]))} unless parent['parent'][0] == company_name || parent['parent'][0] == nil
         #results["parent"+(index+1).to_s][:description] = self.get_description(get_id(parent['parent'][0])) unless parent['parent'][0] == company_name || parent['parent'][0] == nil
       end
 
