c920fc69db5baa52a4908ed0ef459368aa90e4d4
Keaty <holla@keaty.com>
2014-05-27 16:20:39 -0400

    changes to UI
    * added toggle function to long results text
    * began changing main.scss to reflect new branding

--- app/services/nytimes_messenger.rb
+++ app/services/nytimes_messenger.rb
@@ -1,17 +1,17 @@
 class NytimesMessenger
   def format_search(query)
     query.chomp!(" co")
-    query_formatted = query.gsub(" ", "+")
+    query_formatted = query.gsub(" ", "+AND+")
   end
 
   def create_query(query)
-    return ("http://api.nytimes.com/svc/search/v2/articlesearch.json?&fq=document_type:(article)+AND+subject.contains:(Environment+Obesity+Rights+Labor+Cruelty)+AND+organizations:(" + self.format_search(query) + ")&fl=headline,web_url,pub_date&api-key=9f7876895414dc78acc8fe1c9a0dbd03:16:63558649")
+    return ("http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=" + self.format_search(query) + "+AND+document_type:(article)+AND+subject.contains:(Environment+Obesity+Rights+Labor+Cruelty)&fl=headline,web_url,pub_date,keywords&api-key=9f7876895414dc78acc8fe1c9a0dbd03:16:63558649")
   end
 
   def format_response(results)
     results.to_hash.symbolize_keys[:response]
   end
-  
+
   def make_query(query)
     response = HTTParty.get(self.create_query(query))
     self.format_response(response)
